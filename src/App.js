3d1d908a6be3d8763aa715d1753de2940039aea8
Divsolanki <divyanshusolanki061@gmail.com>
2024-09-06 16:23:02 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import UsersTable from './Components/UsersTable';
 import UserDetail from './Components/UserDetail'; 
-import Home from './Components/Home'; 
+// import Home from './Components/Home'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/users" />} />
         <Route path="/users" element={<UsersTable />} />
         <Route path="/users/:id" element={<UserDetail />} />
       </Routes>
--- src/Components/Home.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-const Home = () => {
-  return <h1 className='justify-center text-center mt-7'>Welcome to the D2K Technologies!</h1>;
-};
-
-export default Home;
-
